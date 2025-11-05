57928204547459e3ba05651dfa18e3d94627d1ba
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2025-11-05 11:45:33 +0100

    Use modelctl to wait for components (#7)
    * Use modelctl to wait for components
    
    * Disable read-all for store approval
    
    * Bump CLI to beta.23

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,47 +1,4 @@
 #!/bin/bash -eu
 
-check_missing_components() {
-    local -n missing=$1 # nameref for output array
-    local required="$2"
-
-    missing=()
-    for component in $required; do
-        if [ ! -d "$SNAP_COMPONENTS/$component" ]; then
-            missing+=("$component")
-        fi
-    done
-}
-
-wait_for_components() {
-    local required_components
-
-    required_components="$(modelctl show-engine | yq .components[])"
-
-    missing_components=()
-    check_missing_components missing_components "$required_components"
-
-    max=3600 # seconds
-    interval=10
-    elapsed=0
-    while [ ${#missing_components[@]} -ne 0 ] && [ "$elapsed" -lt $max ]; do
-        ((elapsed+=interval))
-        echo "Waiting for required snap components: [${missing_components[*]}] ($elapsed/${max}s)"
-        sleep "$interval"
-
-        check_missing_components missing_components "$required_components"
-    done
-
-    if [ ${#missing_components[@]} -ne 0 ]; then
-        echo "Error: timed out after ${elapsed}s while waiting for required components: [${missing_components[*]}]"
-        echo "Please use \"snap changes\" to monitor the progress and start the service once all components are installed."
-
-        # Stop service to avoid indefinite retries by systemd, until the next reboot
-        snapctl stop modelctl.server
-        exit 1
-    fi
-}
-
-wait_for_components
-
 engine="$(modelctl show-engine | yq .name)"
-modelctl run "$SNAP/engines/$engine/server"
+modelctl run "$SNAP/engines/$engine/server" --wait-for-components
